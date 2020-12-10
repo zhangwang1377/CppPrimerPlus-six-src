ed653f91cfc0e68b9b6ab37c2c6912d8409ad3ef
luozhangwang <zhangwang1377@163.com>
2020-12-10 21:08:55 +0800

    add chapter_04

---
+++ chapter_04/05_instr3.cpp
@@ -0,0 +1,20 @@
+// instr3.cpp -- reading more than one word with get() & get() 
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    using namespace std;
+
+    const int ArSize = 20;
+    char name[ArSize];
+    char dessert[ArSize];
+
+    cout << "Enter your name:\n";
+    cin.get(name, ArSize).get();
+    cout << "Enter your favorite dessert:\n";
+    cin.get(dessert, ArSize).get();
+    cout << "I have some delicious " << dessert;
+    cout << " for you, " << name << ".\n";
+    
+    return 0;
+}
